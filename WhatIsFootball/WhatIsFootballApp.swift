b927c75d1523182c88d722d2b08120284d44aefa
JosiahCarver <josiah.carver@icloud.com>
2025-10-28 22:40:12 -0400

    Initial Commit

---
+++ WhatIsFootball/WhatIsFootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIsFootballApp.swift
+//  WhatIsFootball
+//
+//  Created by Josiah Carver on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatIsFootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
